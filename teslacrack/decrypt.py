2a4af27cc02007ed5f08df0a224a17a5d2f7a518
Kostis Anagnostopoulos <ankostis@gmail.com>
2016-02-14 21:16:17 +0100

    decrypt.py: remove main() code 9now in __main__.py).

--- teslacrack/decrypt.py
+++ teslacrack/decrypt.py
@@ -15,61 +15,7 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 """
-TeslaCrack - decryptor for the TeslaCrypt ransomware
-
-by Googulator
-
-USAGE: teslacrack.py [-h] [-v] [-n] [--delete] [--delete-old] [--progress]
-                     [--version] [--fix [<.ext>] | --overwrite [<.ext>]]
-                     [fpaths [fpaths ...]]
-
-TeslaCrack - decryptor for the TeslaCrypt ransomware by Googulator
-
-positional arguments:
-  fpaths                Decrypt but don't Write/Delete files, just report
-                        actions performed [default: ['.']].
-
-optional arguments:
-  -h, --help            show this help message and exit
-  -v, --verbose         Verbosely log(DEBUG) all actions on files decrypted.
-  -n, --dry-run         Decrypt but don't Write/Delete files, just report
-                        actions performed [default: False].
-  --delete              Delete crypted-files after decrypting them.
-  --delete-old          Delete crypted even if decrypted-file created during a
-                        previous run [default: False].
-  --progress            Before start decrypting files, pre-scan all dirs, to
-                        provide progress-indicator [default: False].
-  --version             show program's version number and exit
-  --fix [<.ext>]        Re-decrypt tesla-files and overwrite crypted-
-                        counterparts if they have unexpected size. By default,
-                        backs-up existing files with '.BAK' extension. Specify
-                        empty('') extension for no backup (eg. `--fix=`)
-                        WARNING: You may LOOSE FILES that have changed due to
-                        regular use, such as, configuration-files and
-                        mailboxes! [default: False].
-  --overwrite [<.ext>]  Re-decrypt ALL tesla-files, overwritting all crypted-
-                        counterparts. Optionally creates backups with the
-                        given extension. WARNING: You may LOOSE FILES that
-                        have changed due to regular use, such as,
-                        configuration-files and mailboxes! [default: False].
-
-EXAMPLES:
-
-   python teslacrack -v                      ## Decrypt current-folder, logging verbosely.
-   python teslacrack .  bar\cob.xlsx         ## Decrypt current-folder & file
-   python teslacrack --delete-old C:\\       ## WILL DELETE ALL `.vvv` files on disk!!!
-   python teslacrack --progress -n -v  C:\\  ## Just to check what actions will perform.
-
-NOTES:
-
-This script requires pycrypto to be installed.
-
-To use, factor the 2nd hex string found in the headers of affected files using msieve.
-The AES-256 key will be one of the factors, typically not a prime - experiment to see which one works.
-Insert the hex string & AES key below, under known_AES_key_pairs, then run on affected directory.
-If an unknown key is reported, crack that one using msieve, then add to known_AES_key_pairs and re-run.
-
-Enjoy! ;)
+Decrypts TeslaCrypt-ed files.
 """
 
 from __future__ import unicode_literals
@@ -324,57 +270,6 @@ def _path_to_ulong(path):
     return path
 
 
-def _argparse_ext_type(ext):
-    ext = ext.strip()
-    if ext == '': # User wanted option enabled, but without .ext.
-        ext = True
-    elif not ext.startswith('.'):
-        raise argparse.ArgumentTypeError(
-                "Extension %r must start with a dot(`.`) or '' for None!" % ext)
-    return ext
-
-
-def _parse_args(args):
-    doclines = __doc__.split('\n')
-    ap = argparse.ArgumentParser(description='\n'.join(doclines[:4]),
-            epilog='\n'.join(doclines[-12:]))
-    ap.add_argument('-v', '--verbose', action='store_true',
-            help="Verbosely log(DEBUG) all actions on files decrypted.")
-    ap.add_argument('-n', '--dry-run', action='store_true',
-            help="Decrypt but don't Write/Delete files, just report actions performed "
-            "[default: %(default)r].")
-    xgroup_delete = ap.add_mutually_exclusive_group()
-    xgroup_delete.add_argument('--delete', action='store_true',
-            help="Delete crypted-files after decrypting them.")
-    xgroup_delete.add_argument('--delete-old', action='store_true',
-            help="Delete crypted even if decrypted-file created during a previous run "
-            "[default: %(default)r].")
-    ap.add_argument('--progress', action='store_true',
-            help="Before start decrypting files, pre-scan all dirs, to provide progress-indicator "
-            "[default: %(default)r].")
-    ap.add_argument('fpaths', nargs='*', default=['.'],
-            help="Decrypt but don't Write/Delete files, just report actions performed "
-            "[default: %(default)r].")
-    ap.add_argument('--version', action='version', version='%(prog)s 2.0')
-    xgroup_overwrite = ap.add_mutually_exclusive_group()
-    xgroup_overwrite.add_argument('--fix', nargs='?',
-            type=_argparse_ext_type, metavar='<.ext>', default=False, const='.BAK',
-            help="Re-decrypt tesla-files and overwrite crypted-counterparts if they have unexpected size. "
-            "By default, backs-up existing files with '%(const)s' extension. "
-            "Specify empty('') extension for no backup (eg. `--fix=`) "
-            "WARNING: You may LOOSE FILES that have changed due to regular use, "
-            "such as, configuration-files and mailboxes! "
-            "[default: %(default)s]. ")
-    xgroup_overwrite.add_argument('--overwrite', nargs='?',
-            type=_argparse_ext_type, metavar='<.ext>', default=False, const=True,
-            help="Re-decrypt ALL tesla-files, overwritting all crypted-counterparts. "
-            "Optionally creates backups with the given extension. "
-            "WARNING: You may LOOSE FILES that have changed due to regular use, "
-            "such as, configuration-files and mailboxes! "
-            "[default: %(default)s]. ")
-    return ap.parse_args(args)
-
-
 def decrypt(opts):
     opts.fpaths = [_path_to_ulong(f) for f in opts.fpaths]
 
@@ -393,16 +288,4 @@ def decrypt(opts):
 
     return stats
 
-def main(*args):
-    """Parse args, setup logging and delegate to :func:`teslacrack()`."""
-    if not args:
-        args = sys.argv
-    opts = _parse_args(args[1:])
 
-    log_level = logging.DEBUG if opts.verbose else logging.INFO
-    init_logging(log_level)
-    log.debug('Options: %s', opts)
-    decrypt(opts)
-
-if __name__=='__main__':
-    main()
