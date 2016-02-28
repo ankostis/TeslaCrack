c783b2c97fcd028da51e16e577acd953ec13f25e
Kostis Anagnostopoulos <ankostis@gmail.com>
2016-02-29 00:04:35 +0100

    Addd TC for AES-ecdsa key unfactor.

--- tests/test_unfactor.py
+++ tests/test_unfactor.py
@@ -154,7 +154,7 @@ class TUnfactor(unittest.TestCase):
 #             self.assertIn(key_rec['error'], err_msg, msg=key_rec)
 
     @ddt.data(*[k for k in app_db['keys'] if k['type'] == 'BTC'])
-    def test_unfactor_ecdsa_btc_from_file(self, key_rec):
+    def test_unfactor_ecdsa_BTC_from_file(self, key_rec):
         for f in key_rec.get('crypted_files', ()):
             exp_aes_key = key_rec.get('decrypted')
             if not exp_aes_key:
@@ -162,9 +162,18 @@ class TUnfactor(unittest.TestCase):
             factors = [int(fc) for fc in key_rec['factors']]
             key_name, key = unfactor.crack_ecdsa_key_from_file(f, factors)
             #print(key_rec['name'], f, aes_keys, exp_aes_key)
-            self.assertEqual('BTC', key_name, msg=key_rec)
             self.assertIn(exp_aes_key.upper(), '0x%064X'%key, msg=key_rec)
 
+    @ddt.data(*[k for k in app_db['keys'] if k['type'] == 'AES'])
+    def test_unfactor_ecdsa_AES_from_file(self, key_rec):
+        for f in key_rec.get('crypted_files', ()):
+            exp_aes_key = key_rec.get('decrypted')
+            if not exp_aes_key:
+                continue
+            factors = [int(fc) for fc in key_rec['factors']]
+            key_name, key = unfactor.crack_ecdsa_key_from_file(f, factors)
+            #print(key_rec['name'], f, aes_keys, exp_aes_key)
+            self.assertIn(exp_aes_key.upper(), '0x%064X'%key, msg=key_rec)
 
     @ddt.data(*[k for k in app_db['keys'] if k['type'] == 'BTC'])
     def test_unfactor_btc_address(self, key_rec):
